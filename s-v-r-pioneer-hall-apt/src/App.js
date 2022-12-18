32226915ca3489e52c97c810dc3fb0beb41a7781
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2022-12-18 11:08:27 -0700

    spa app working. Needs style

--- s-v-r-pioneer-hall-apt/src/App.js
+++ s-v-r-pioneer-hall-apt/src/App.js
@@ -1,32 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from './Components/Menu/Menu'
 import Home from './Components/Home/Home';
 
 import Footer from './Components/Footer/Footer'
-import ImageViwer from './Components/ImageViewer/ImageViewer.jsx';
 import Room from './Components/Room/Room';
 import Building from './Components/Building/Building';
 import ContactForm from './Components/ContactForm/ContactForm';
+import { useState } from 'react';
 
 function App() {
+  const [page, setPage]= useState("/rooms")
+console.log(page)
   return (
     <div className="App">
-      <Menu></Menu>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<Room/>}/>
-          <Route path="/building" element={<Building/>}/>
-          <Route path="/contact" element={<ContactForm/>}/>
-          <Route path="/apply" element={<Room/>}/>
-          <Route path="/payrent" element={<Room/>}/>
-          <Route path="/aboutus" element={<Room/>}/>
-          
-        </Routes>
-      </BrowserRouter>
+      <Menu parentCallBack = {setPage}></Menu>
+      {page==="/"&&
+      <Home />
+      }
+      {page==="/rooms"&&
+      <Room />
+      }
+      {page==="/building"&&
+      <Building />
+      }
+      {page==="/contact"&&
+      <ContactForm />
+      }
+      
       <Footer></Footer>
     </div>
   );
--- s-v-r-pioneer-hall-apt/src/Components/MenuContent/MenuContent.jsx
+++ s-v-r-pioneer-hall-apt/src/Components/MenuContent/MenuContent.jsx
@@ -1,9 +1,13 @@
 import data from './MenuContentData.json';
+import links from './MenuLinks.json';
 import './style.css';
-function MenuContent() {
+function MenuContent(params) {
+
+
     return (<div id="MenuContent">
         <ul id="MenuContentul">
-            {data && data.map(item => (<a href={item.link} key={String(item.name)}>{item.name}</a>))}
+            {data && data.map(item => (<button  key={String(item.name)} onClick={()=>(params.parentCallBack(item.link))}>{item.name}</button>))}
+            {links && links.map(item => (<a  href={item.link} key={String(item.name)} onClick={()=>(params.parentCallBack(item.link))}>{item.name}</a>))}
         </ul>
     </div>)
 }
